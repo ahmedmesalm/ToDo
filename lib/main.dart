fba05284920d5b8aa353221b2a5d2d83acb89a82
Mesalm <ahmedmesalam96@gmail.com>
2023-11-30 00:22:48 +0200

    create a Theme App , HomeScreenTabs

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:todo/ui/home/home_screen.dart';
+import 'package:todo/ui/utils/themeApp.dart';
 
 void main (){
   runApp(const MyApp()) ;
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeApp.lightTheme,
       initialRoute: HomeScreen.routeName,
       routes: {
         HomeScreen.routeName : (context) => HomeScreen() ,
--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:todo/ui/home/settings/settings.dart';
+import 'package:todo/ui/home/task_list/tasks_list.dart';
 
-class HomeScreen extends StatelessWidget {
+
+class HomeScreen extends StatefulWidget {
   static const routeName = " home screen" ;
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int selected = 0 ;
+
+  List<Widget> Tabs = [TasksList() , Setting()] ;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('To Do List' ,style: Theme.of(context).textTheme.titleLarge,),
+      ),
+      body: Tabs[selected],
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        notchMargin: 8,
+        child: BottomNavigationBar(
+          currentIndex: selected,
+          onTap: (index){
+            index = selected ;
+            setState(() {});
+          },
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.list_rounded,size: 30,),
+                label: "Tasks List"),
+            BottomNavigationBarItem(icon:Icon(Icons.settings,size: 30,)
+                ,label: "Settings")
+
+          ],),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+      child: Icon(Icons.add),),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
 
     );
   }
